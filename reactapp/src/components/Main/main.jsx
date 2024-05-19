4cc353071d8dee1a3dfc10291d57df0bc5a963bf
razumm56 <egor.konechnykh@gmail.com>
2024-05-19 12:44:27 +0500

    update reactapp

---
+++ reactapp/src/components/Main/main.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useRef, useState} from "react";
+import { motion } from "framer-motion";
+import "./style.css";
+import Diagrams from "../diagrams/Diagrams";
+const Main = () => {
+	const inputRef = useRef();
+	const [inputValue, setInputValue] = useState('')
+	const handleInputChange = (event) => {
+		event.preventDefault();
+		// ВОТ ЗДЕСЬ БУДЕТ ДЕЛАТЬ ЗАПРОС НА СЕРВЕР
+		setInputValue(inputRef.current.value);
+		console.log("Значение:", inputRef.current.value);
+		inputRef.current.value = "";
+	};
+	return (
+		<motion.div
+			intial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0, transition: { duration: 0.2 } }}
+		>
+			{/* <Diagrams/> */}
+			<div className="main">
+				<Diagrams className="diogram" />
+				{/* <Diagrams2 className = 'diogram' /> */}
+				<p>{inputValue}Нейтральных комментариев больше. Всего комментариев: 58</p>
+				<form>
+					<input
+						className="main_input"
+						type="text"
+						ref={inputRef}
+						placeholder="Введите ссылку"
+					/>
+					<button className="btn1" onClick={handleInputChange}>
+						➤
+					</button>
+				</form>
+			</div>
+		</motion.div>
+	);
+};
+
+export default Main;
---
+++ reactapp/src/components/diagrams/Diagrams.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import { PieChart, Pie, Tooltip } from "recharts";
+
+const Diagrams = () => {
+	const [windowSize, setWindowSize] = useState({
+		width: window.innerWidth,
+		height: window.innerHeight,
+	});
+
+	useEffect(() => {
+		const handleResize = () =>
+			setWindowSize({
+				width: window.innerWidth,
+				height: window.innerHeight,
+			});
+
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
+
+	// Функция для определения размеров графика
+	const getChartDimensions = () => {
+		if (windowSize.width < 768) {
+			return { width: 400, height: 400, outerRadius: "80%" };
+		} else {
+			return { width: 550, height: 500, outerRadius: "90%" };
+		}
+	};
+
+	const chartDimensions = getChartDimensions();
+	const data01 = [
+		{ name: "К-во положительных комментариев", value: 1, fill: "#4caf50" },
+		{ name: "К-во отрицательных комментариев", value: 2, fill: "#f44336" },
+		{ name: "К-во нейтральных комментариев", value: 55, fill: "#2196f3" },
+	];
+
+	return (
+		<div className="piechart-container">
+			<PieChart
+				width={chartDimensions.width}
+				height={chartDimensions.height}
+			>
+				<Pie
+					dataKey="value"
+					isAnimationActive={true}
+					data={data01}
+					cx="50%"
+					cy="50%"
+					outerRadius={chartDimensions.outerRadius}
+					fill="#8884d8"
+					label
+				/>
+				<Tooltip />
+			</PieChart>
+		</div>
+	);
+};
+
+export default Diagrams;
