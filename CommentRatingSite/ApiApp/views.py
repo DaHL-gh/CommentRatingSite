2fcd2b523f5888a5636ffb76c701e9c151d47ccc
DaHL <8tima18@gmail.com>
2024-05-24 18:27:57 +0500

    moved AIModel in django file first version of api

--- CommentRatingSite/ApiApp/views.py
+++ CommentRatingSite/ApiApp/views.py
@@ -3,10 +3,22 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.views import APIView
 
+from AIModel import parser_comments, model_using
 
 class AiModelView(APIView):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
     def post(self, request: Request):
+        vkapp = parser_comments.VkApp()
 
-        answer = request.data
+        answer = {'positive': 0, 'neutral': 0, 'negative': 0, 'count': 0}
 
+        comments = vkapp.get(request.data['url'])
+
+        for com in comments:
+            result = model_using.query(com)[0]
+            if com != '':
+                answer[result] += 1
+                answer['count'] += 1
         return Response(answer)
