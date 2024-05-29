045918e44d621357a489f91bb0cf421876773c94
Senox <very-nice96@mail.ru>
2024-05-29 10:01:32 +0500

    some change for model opp

--- CommentRatingSite/ApiApp/views.py
+++ CommentRatingSite/ApiApp/views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.views import APIView
 
-from AIModel import parser_comments, model_using
+from CommentRatingSite.AIModel.model_using import Model
+
 
 class AiModelView(APIView):
     def __init__(self, *args, **kwargs):
@@ -11,14 +12,10 @@ class AiModelView(APIView):
 
     def post(self, request: Request):
         vkapp = parser_comments.VkApp()
-
-        answer = {'positive': 0, 'neutral': 0, 'negative': 0, 'count': 0}
+        model = Model()
 
         comments = vkapp.get(request.data['url'])
 
-        for com in comments:
-            result = model_using.query(com)[0]
-            if com != '':
-                answer[result] += 1
-                answer['count'] += 1
-        return Response(answer)
+        return Response(model.predict_list_comments(comments))
+
+
