0c6c40b7f1ce52f1a9231edee24acbb806914d04
Senox <blya>
2024-06-03 09:56:45 +0500

    add work parser

--- CommentRatingSite/ApiApp/views.py
+++ CommentRatingSite/ApiApp/views.py
@@ -4,6 +4,7 @@ from rest_framework.request import Request
 from rest_framework.views import APIView
 
 from CommentRatingSite.AIModel.model_using import Model
+from CommentRatingSite.AIModel.parser_comments import VkApp
 
 
 class AiModelView(APIView):
@@ -11,11 +12,10 @@ class AiModelView(APIView):
         super().__init__(*args, **kwargs)
 
     def post(self, request: Request):
-        vkapp = parser_comments.VkApp()
+        vkapp = VkApp()
         model = Model()
 
         comments = vkapp.get(request.data['url'])
 
         return Response(model.predict_list_comments(comments))
-
 
