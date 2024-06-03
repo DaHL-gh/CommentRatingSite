35bcfccd3f04ca33c0acb8105b8a9ca2ea259044
DaHL <8tima18@gmail.com>
2024-06-03 12:00:52 +0500

    some changes in model_using.py

--- CommentRatingSite/AIModel/model_using.py
+++ CommentRatingSite/AIModel/model_using.py
@@ -12,19 +12,18 @@ class Model:
         self.label_encoder = load(self.module_dir + r"\label_encoder.bin")
         self.model = load(self.module_dir + r"\model_v_0_1.pkl")
 
-    def predict_single_comment(self, comment: list) -> str:
-        clear_comment = self.komm_cleaner.clean_text(''.join(comment))
+    def predict_single_comment(self, comment: str) -> str:
+        clear_comment = self.komm_cleaner.clean_text(comment)
         vect_comment = self.tfidf_vectorizer.transform([clear_comment])
         return self.label_encoder.inverse_transform(self.model.predict(vect_comment))
 
     def predict_list_comments(self, list_of_comments: list) -> dict:
-        count_com, pos_com, neg_com, neut_com = 0, 0, 0, 0
+        count_com = pos_com = neg_com = neut_com = 0
+
         for comment in list_of_comments:
             count_com += 1
-            clear_comment = self.komm_cleaner.clean_text(comment)
-            vect_comment = self.tfidf_vectorizer.transform([clear_comment])
 
-            predict = self.label_encoder.inverse_transform(self.model.predict(vect_comment))
+            predict = self.predict_single_comment(comment)
 
             if predict == 'positive':
                 pos_com += 1
